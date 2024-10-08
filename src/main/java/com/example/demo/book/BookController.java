cff284f6766d58ef908e7f4787289324e704ae5c
davletmegahorosh <azimovdavlet460@gmail.com>
2024-10-08 21:27:52 +0600

    finished tutorial

--- src/main/java/com/example/demo/book/BookController.java
+++ src/main/java/com/example/demo/book/BookController.java
@@ -26,4 +26,18 @@ public class BookController {
         bookService.addNewBook(book);
     }
 
+    @DeleteMapping(path = "{book_id}")
+    public void deleteBook(@PathVariable("book_id") Long book_id){
+        bookService.deleteBook(book_id);
+    }
+
+    @PutMapping(path = "{book_id}")
+    public void updateBook(
+            @PathVariable("book_id") Long book_id,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) int pages,
+            @RequestParam(required = false) int price,
+            @RequestParam(required = false) String author){
+        bookService.updateBook(book_id, name, pages, price, author);
+    }
 }
