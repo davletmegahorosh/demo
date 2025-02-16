3f269e33e3dcd4b05c93cae7aa9080ad829f1879
davletmegahorosh <davletazimov142@gmail.com>
2025-02-16 18:41:48 +0600

    .env

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
